e9b761a8b757cde9f140b204156636ccce03d0b6
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2017-01-14 16:32:21 +0200

    session fix

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+?>
 <!DOCTYPE html>
 <?php require_once 'sessionSettings.php'; ?>
 <?php
@@ -5,23 +10,22 @@ if (isset($_POST['login'])) {
     require_once 'login.php';
 } else if (isset($_POST['logout'])) {
     require_once 'logout.php';
-} else if (isset($_GET['setStyle'])){
+} else if (isset($_GET['setStyle'])) {
     require_once 'setStyle.php';
 }
-
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <?php require 'sessionSettings_getStyleTags.php'; ?>
+<?php require 'sessionSettings_getStyleTags.php'; ?>
         <title></title>
     </head>
     <body>
         <div class="topnav">
-            <?php require 'index_topnav.php'; ?>
+<?php require 'index_topnav.php'; ?>
         </div>
         <div class="content">
-            <?php require 'index_content.php'; ?>
+<?php require 'index_content.php'; ?>
         </div>
     </body>
 </html>
--- sessionSettings.php
+++ sessionSettings.php
@@ -1,4 +1,3 @@
-<?php require_once 'sessionSettings.php'; ?>
 <?php
 
 // session initialization:
